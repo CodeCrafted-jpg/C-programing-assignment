be233a5d6687c745a85ac3166acc78b3cf5a916c
Sayan Mallick <malsayan6@gmail.com>
2026-01-15 10:16:32 +0530

    16

--- Assignment 2/16.c
+++ Assignment 2/16.c
@@ -1,37 +1,37 @@
 // 16. Write a program in C to find the second largest element in an array.
+
 #include <stdio.h>
 
 int main() {
     int arr[100], n, i;
-    int max,sec_lar;
+    int max, sec_lar;
 
     printf("Enter the number of elements: ");
     scanf("%d", &n);
+
+    if (n < 2) {
+        printf("Second largest element not possible.");
+        return 0;
+    }
 
     printf("Enter %d elements:\n", n);
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    max = arr[0];
 
-    for (i = 1; i < n; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-      
-    }
-
-
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i]<max && arr[i]>arr[i+1] );
-        {
-            sec_lar=arr[i];
-            printf("%d", sec_lar);
-            return 0;
-        }
-        
-    }
     
- 
+    max = sec_lar = -99999;
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] > max) {
+            sec_lar = max;
+            max = arr[i];
+        } else if (arr[i] > sec_lar && arr[i] < max) {
+            sec_lar = arr[i];
+        }
+    }
+
+    printf("Second largest element = %d", sec_lar);
+
+    return 0;
 }
