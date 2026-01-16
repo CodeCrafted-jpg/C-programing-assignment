8e8c1d5fe0efbfa7591f24ab4f188a761543c7bb
Sayan Mallick <malsayan6@gmail.com>
2026-01-16 20:43:21 +0530

    21

--- Assignment 2/21.c
+++ Assignment 2/21.c
@@ -3,38 +3,38 @@
 #include <stdio.h>
 
 int main() {
-    int a[10][10], b[10][10], sum[10][10];
+    int a[10][10], b[10][10], mul[10][10];
     int r, c, i, j;
 
-    printf("Enter number of rows and columns: ");
-    scanf("%d %d", &r, &c);
+    printf("Enter number of rows for the square matrix: ");
+    scanf("%d", &r);
 
     printf("Enter elements of first matrix:\n");
     for (i = 0; i < r; i++) {
-        for (j = 0; j < c; j++) {
+        for (j = 0; j < r; j++) {
             scanf("%d", &a[i][j]);
         }
     }
 
     printf("Enter elements of second matrix:\n");
     for (i = 0; i < r; i++) {
-        for (j = 0; j < c; j++) {
+        for (j = 0; j < r; j++) {
             scanf("%d", &b[i][j]);
         }
     }
 
   
     for (i = 0; i < r; i++) {
-        for (j = 0; j < c; j++) {
-            sum[i][j] = a[i][j] - b[i][j];
+        for (j = 0; j < r; j++) {
+            mul[i][j] = a[i][j] * b[i][j];
         }
     }
 
     
-    printf("Difference of the two matrices:\n");
+    printf("multiplication of the two matrices:\n");
     for (i = 0; i < r; i++) {
-        for (j = 0; j < c; j++) {
-            printf("%d\t", sum[i][j]);
+        for (j = 0; j < r; j++) {
+            printf("%d\t", mul[i][j]);
         }
         printf("\n");
     }
